1777b61ca5d3a9d07e3646ce0fa8ad1ded5c455f
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-09-17 11:08:45 +0200

    update styles and ratings

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,45 @@
 <script>
 import axios from "axios";
+
 import { store } from "./store.js";
 import AppHeader from "./components/AppHeader.vue";
 import CardList from "./components/cardList.vue";
 import CardItems from "./components/CardItems.vue";
+
 export default {
   components: {
     AppHeader,
     CardList,
     CardItems,
   },
+
   // star data
   data() {
     return {
       store,
     };
   },
+
   // start methods
   methods: {
-    // getInfoMovie() {
-    // },
-    // getInfoTv() {
-    // },
     getCards() {
-      // this.getInfoMovie();
-      // this.getInfoTv();
       // Movie
       let searchUrlMovie = store.apiUrlMovie + store.titleSearched;
+
       axios.get(searchUrlMovie).then((ref) => {
         store.movieList = ref.data.results;
-        // console.log(store.movieList);
         // cancat array movie end tv
         store.cardList = store.tvList.concat(store.movieList);
-        console.log(store.cardList);
+        // console.log(store.cardList);
       });
+      // serieTv
+      let searchUrlTv = store.apiUrlTv + store.titleSearched;
       axios.get(searchUrlTv).then((ref) => {
         store.tvList = ref.data.results;
         // console.log(store.tvList);
         // cancat array movie end tv
         store.cardList = store.movieList.concat(store.tvList);
-        console.log(store.cardList);
+        // console.log(store.cardList);
       });
     },
   },
@@ -48,14 +48,17 @@ export default {
   },
 };
 </script>
+
 <template>
   <AppHeader @search="getCards" />
   <main>
     <CardList />
   </main>
 </template>
+
 <style lang="scss">
 @use "./styles/generals.scss" as *;
+
 main {
   margin: 20px;
 }
--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
 import { reactive } from "vue";
+
 export const store = reactive({
   // API URL
   apiUrlMovie:
-    "https://api.themoviedb.org/3/search/movie?api_key=4354bdc1164accce56e509efe2a30375&query=",
+    "https://api.themoviedb.org/3/search/movie?api_key=4354bdc1164accce56e509efe2a30375&language=it_IT&query=",
   apiUrlTv:
-    "https://api.themoviedb.org/3/search/tv?api_key=4354bdc1164accce56e509efe2a30375&query=",
+    "https://api.themoviedb.org/3/search/tv?api_key=4354bdc1164accce56e509efe2a30375&language=it_IT&query=",
+
   // variables to search
-  titleSearched: "ritorno al futuro",
+  titleSearched: "",
   movieList: [],
   tvList: [],
   cardList: [],
-  thumbImg: "https://image.tmdb.org/t/p/w342",
+  thumbImg: "http://image.tmdb.org/t/p/original/",
 });
